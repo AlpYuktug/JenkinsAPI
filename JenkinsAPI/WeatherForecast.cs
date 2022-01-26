3845af98947627a9faf0aa1652065efab4e273ea
Alperen Yüksel Altuğ <alperen.altug@gmail.com>
2022-01-26 11:49:52 +0300

    hijenkins

--- JenkinsAPI/WeatherForecast.cs
+++ JenkinsAPI/WeatherForecast.cs
@@ -11,6 +11,6 @@ namespace JenkinsAPI
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
-        public string Summaryw { get; set; }
+        public string Summaryasd { get; set; }
     }
 }
